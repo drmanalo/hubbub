12c3108a0a5007afd03baed6d11e104f042413fc
drmanalo <drmanalo@outlook.com>
2015-07-23 06:46:02 +0100

    unversioned out folder

--- grails-app/controllers/com/grailsinaction/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -1,5 +1,37 @@
 package com.grailsinaction
 
 class PostController {
+
     static scaffold = true
+
+    def index() {
+        if (!params.id) {
+            params.id = "chuck_norris"
+        }
+        redirect(action: 'timeline', params: params)
+    }
+
+    def timeline() {
+        def user = User.findByLoginId(params.id)
+        if (!user) {
+            response.sendError(404)
+        } else {
+            [ user : user ]
+        }
+    }
+
+    def addPost() {
+        def user = User.findByLoginId(params.id)
+        if (user) {
+            def post = new Post(params)
+            user.addToPosts(post)
+            if (user.save()) {
+                flash.message = "Successfully created post"
+            } else {
+                flash.message = "Invalid or empty post"
+            }
+        }
+        redirect(action: 'timeline', id: params.id)
+    }
+
 }
